f6d6efbc8bbffec675adccf8e22583ac62739285
Fabian Meyer <user8324@posteo.net>
2018-06-18 09:54:22 +0200

    Rename constaint to error function

---
+++ src/optcpp/error_function.h
@@ -0,0 +1,42 @@
+/*
+ * error_function.h
+ *
+ *  Created on: 04 May 2018
+ *      Author: Fabian Meyer
+ */
+
+#ifndef OPT_ERROR_FUNCTION_H_
+#define OPT_ERROR_FUNCTION_H_
+
+#include <Eigen/Dense>
+
+namespace opt
+{
+    /** Interface to define error functions for optimization problems. */
+    class ErrorFunction
+    {
+    public:
+        ErrorFunction() { }
+        virtual ~ErrorFunction() { }
+
+        struct Result
+        {
+            /** Function value. */
+            Eigen::VectorXd val;
+            /** Function jacobian. */
+            Eigen::MatrixXd jac;
+        };
+
+        /** Returns the length of the output vector of the error function.
+          * This is used for prediction the length of the final function vetcor
+          * @return length of the result vector */
+        virtual unsigned int dimension() const = 0;
+
+        /**
+         * Evaluates the error function and its jacobian.
+         * @return struct containing function value and jacobian */
+        virtual Result eval(const Eigen::VectorXd &state) const = 0;
+    };
+}
+
+#endif
