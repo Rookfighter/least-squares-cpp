467a96a8fa21f5476e62dd46dd0b7a42b7a95f40
Fabian Meyer <user8324@posteo.net>
2018-08-14 17:12:06 +0200

    Fix math issues with other compilers than gcc

---
+++ include/optcpp/math.h
@@ -0,0 +1,40 @@
+/*
+ * math.h
+ *
+ *  Created on: 04 May 2018
+ *      Author: Fabian Meyer
+ */
+
+#ifndef OPT_MATH_H_
+#define OPT_MATH_H_
+
+#include <cmath>
+
+namespace opt
+{
+    double pi()
+    {
+        return std::atan(1.0) * 4.0;
+    }
+
+    inline bool equals(double a, double b, double eps)
+    {
+        return std::abs(a - b) <= eps;
+    }
+
+    inline bool iszero(double a, double eps)
+    {
+        return equals(a, 0.0, eps);
+    }
+
+    inline double normalizeAngle(double angle)
+    {
+        while(angle <= -pi())
+            angle += 2 * pi();
+        while(angle > pi())
+            angle -= 2 * pi();
+        return angle;
+    }
+}
+
+#endif
