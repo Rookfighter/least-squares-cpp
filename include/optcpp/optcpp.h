fea0e561433508d8966ac82a9abbf808a3df5be4
Fabian Meyer <user8324@posteo.net>
2018-08-14 18:11:46 +0200

    Add collectinh header file

---
+++ include/optcpp/optcpp.h
@@ -0,0 +1,17 @@
+/*
+ * optcpp.h
+ *
+ *  Created on: 14 Aug 2018
+ *      Author: Fabian Meyer
+ */
+
+#ifndef OPT_OPTCPP_H_
+#define OPT_OPTCPP_H_
+
+#include <optcpp/armijo_backtracking.h>
+#include <optcpp/increasing_line_search.h>
+#include <optcpp/gauss_newton.h>
+#include <optcpp/gradient_descent.h>
+#include <optcpp/levenberg_marquardt.h>
+
+#endif
