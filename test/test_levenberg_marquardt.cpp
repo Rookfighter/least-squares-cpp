292fb949563e9636ecc8ea959cbac96d84948e4c
Fabian Meyer <user8324@posteo.net>
2018-08-28 15:56:16 +0200

    Add levenberg marquardt tests

--- test/test_levenberg_marquardt.cpp
+++ test/test_levenberg_marquardt.cpp
@@ -9,6 +9,8 @@
 #include "error_functions.h"
 #include <optcpp/levenberg_marquardt.h>
 
+using namespace opt;
+
 TEST_CASE("Levenberg Marquardt")
 {
     SECTION("with linear error functions")
@@ -28,28 +30,30 @@ TEST_CASE("Levenberg Marquardt")
         eq3->factors.resize(3);
         eq3->factors << 4, -2, 0;
 
-        std::vector<opt::ErrorFunction *> errFuncs = {eq1, eq2, eq3};
-        opt::LevenbergMarquardt lm;
+        std::vector<ErrorFunction *> errFuncs = {eq1, eq2, eq3};
+        LevenbergMarquardt lm;
         lm.setDamping(1.0);
         lm.setErrorFunctions(errFuncs);
         lm.setMaxIterations(10);
         lm.setEpsilon(1e-12);
 
-        // SECTION("optimize")
-        // {
-        //     Eigen::VectorXd state(3);
-        //     state << 3, 2, 1;
-        //     Eigen::VectorXd stateExp(3);
-        //     stateExp << 1, 2, 3;
-        //
-        //     auto result = lm.optimize(state);
-        //     state << result.state(0) / result.state(0),
-        //         result.state(1) / result.state(0),
-        //         result.state(2) / result.state(0);
-        //
-        //     REQUIRE(result.converged);
-        //     REQUIRE(result.iterations == 7);
-        //     REQUIRE_MAT(stateExp, state, eps);
-        // }
+        SECTION("optimize")
+        {
+            Eigen::VectorXd errValA, errValB;
+            Eigen::MatrixXd errJacA, errJacB;
+            Eigen::VectorXd state(3);
+            state << 3, 2, 1;
+            Eigen::VectorXd stateExp(3);
+            stateExp << 1, 2, 3;
+
+            auto result = lm.optimize(state);
+
+            evalErrorFuncs(state, errFuncs, errValA, errJacA);
+            evalErrorFuncs(result.state, errFuncs, errValB, errJacB);
+
+            REQUIRE(result.converged);
+            REQUIRE(result.iterations == 7);
+            REQUIRE(squaredError(errValB) < squaredError(errValA));
+        }
     }
 }
