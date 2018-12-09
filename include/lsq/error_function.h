23a87bf9ed1a812f2e522c5bbd1d0370a2f0952f
Fabian Meyer <user8324@posteo.net>
2018-12-09 14:41:10 +0100

    Make error function templated

--- include/lsq/error_function.h
+++ include/lsq/error_function.h
@@ -12,19 +12,46 @@
 
 namespace lsq
 {
+
+    template<typename Scalar>
+    using Vector = Eigen::Matrix<Scalar, Eigen::Dynamic, 1>;
+
+    template<typename Scalar>
+    using Matrix = Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic>;
+
     /** Interface to define error functions for optimization problems. */
+    template<typename Scalar, typename Error>
     class ErrorFunction
     {
+    private:
+        Scalar fdEps_;
+
     public:
         ErrorFunction()
+            : fdEps_(std::sqrt(std::numeric_limits<double>::epsilon()))
         {}
+
         virtual ~ErrorFunction()
         {}
 
-        void computeFiniteDifferences(const Eigen::VectorXd &state,
-            const Eigen::VectorXd &errValue,
-            Eigen::MatrixXd &outJacobian,
-            const double diff) const
+        void setNumericalEps(const Scalar eps)
+        {
+            fdEps_ = eps;
+        }
+
+        /** Internal evaluation of the error function and its jacobian.
+-         *  If the function calculates no jacobian then finite differences
+-         *  is used to approximate it.
+-         *  @param state current state estimate
+-         *  @param outValue function value of the error function
+-         *  @param outJacobian jacobian of the error function */
+-        virtual void _evaluate(const Eigen::VectorXd &state,
+-            Eigen::VectorXd &outValue,
+-            Eigen::MatrixXd &outJacobian) const = 0;
+
+        void computeFiniteDifferences(const Vector<Scalar> &state,
+            const Vector<Scalar> &errValue,
+            Matrix<Scalar> &outJacobian)
         {
             Eigen::VectorXd stateTmp;
             Eigen::VectorXd errValueTmp;
@@ -35,49 +62,30 @@ namespace lsq
             for(unsigned int i = 0; i < state.size(); ++i)
             {
                 stateTmp = state;
-                stateTmp(i) += diff;
+                stateTmp(i) += fdEps_;
 
-                _evaluate(stateTmp, errValueTmp, errJacobianTmp);
+                errFunctor_(stateTmp, errValueTmp, errJacobianTmp);
                 assert(errValueTmp.size() == errValue.size());
 
                 outJacobian.col(i) = (errValueTmp - errValue) / diff;
             }
         }
 
-        /** Returns the length of the output vector of the error function.
-         *  This is used for prediction the length of the final function vetcor
-         *  @return length of the result vector */
-        virtual size_t dimension() const = 0;
-
-        /** Internal evaluation of the error function and its jacobian.
-         *  If the function calculates no jacobian then finite differences
-         *  is used to approximate it.
-         *  @param state current state estimate
-         *  @param outValue function value of the error function
-         *  @param outJacobian jacobian of the error function */
-        virtual void _evaluate(const Eigen::VectorXd &state,
-            Eigen::VectorXd &outValue,
-            Eigen::MatrixXd &outJacobian) const = 0;
-
         /** Evaluates the error function and its jacobian.
          *  @param state current state estimate
          *  @param outValue function value of the error function
          *  @param outJacobian jacobian of the error function */
-        void evaluate(const Eigen::VectorXd &state,
-            Eigen::VectorXd &outValue,
-            Eigen::MatrixXd &outJacobian) const
+        void evaluate(const Vector<Scalar> &state,
+            Vector<Scalar> &outValue,
+            Matrix<Scalar> &outJacobian)
         {
-            static const double diff = std::sqrt(
-                std::numeric_limits<double>::epsilon());
-
             outJacobian.resize(0, 0);
-            _evaluate(state, outValue, outJacobian);
+            errFunctor_(state, outValue, outJacobian);
 
             // if no jacobian was computed use finite differences
             if(outJacobian.size() == 0)
-                computeFiniteDifferences(state, outValue, outJacobian, diff);
+                computeFiniteDifferences(state, outValue, outJacobian);
 
-            assert(static_cast<size_t>(outValue.size()) == dimension());
             assert(outJacobian.rows() == outValue.size());
             assert(outJacobian.cols() == state.size());
         }
@@ -87,57 +95,10 @@ namespace lsq
      *  vector. Calculates as:  0.5 * err^T * err
      *  @param errorVec vector of error values
      *  @return squared error */
-    inline double squaredError(const Eigen::VectorXd &errorVec)
+    template<typename Scalar>
+    inline Scalar squaredError(const Vector<Scalar> &errorVec)
     {
-        return 0.5 * (errorVec.transpose() * errorVec)(0);
-    }
-
-    inline size_t totalDimension(const std::vector<ErrorFunction *> &errFuncs)
-    {
-        size_t sum = 0;
-        for(const ErrorFunction *e : errFuncs)
-            sum += e->dimension();
-        return sum;
-    }
-
-    /** Calculates the value and jacobians of a vector of error functions.
-     *  @param state current state vector
-     *  @param errFuncs vector of error functions
-     *  @param outValue function value of the error function
-     *  @param outJacobian jacobian of the error function */
-    inline void evalErrorFuncs(const Eigen::VectorXd &state,
-        const std::vector<ErrorFunction *> &errFuncs,
-        Eigen::VectorXd &outValue,
-        Eigen::MatrixXd &outJacobian)
-    {
-        size_t dim = totalDimension(errFuncs);
-        outValue.resize(dim);
-        outJacobian.resize(dim, state.size());
-
-        Eigen::VectorXd errVal;
-        Eigen::MatrixXd errJac;
-        // keep track of the error index since error functions can
-        // return arbitrary amount of values
-        size_t eidx = 0;
-        for(unsigned int i = 0; i < errFuncs.size(); ++i)
-        {
-            const ErrorFunction *errfun = errFuncs[i];
-
-            // calculate error function of the current state
-            errfun->evaluate(state, errVal, errJac);
-
-            for(unsigned int j = 0; j < errVal.size(); ++j)
-                outValue(eidx + j) = errVal(j);
-
-            // copy whole jacobian into one row of coefficient matrix
-            for(unsigned int row = 0; row < errJac.rows(); ++row)
-            {
-                for(unsigned int col = 0; col < errJac.cols(); ++col)
-                    outJacobian(eidx + row, col) = errJac(row, col);
-            }
-
-            eidx += errfun->dimension();
-        }
+        return Scalar(0.5) * errorVec.squaredNorm();
     }
 }
 
