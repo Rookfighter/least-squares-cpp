5b70b858fe10f71b42c82fd42634d4df49bdc003
Fabian Meyer <user8324@posteo.net>
2019-08-05 10:53:41 +0530

    Add test for dense solver

---
+++ test/dense_solver.cpp
@@ -0,0 +1,36 @@
+/* finite_differences.cpp
+ *
+ * Author: Fabian Meyer
+ * Created On: 05 Aug 2019
+ */
+
+#include <lsqcpp.h>
+#include "assert/eigen_require.h"
+
+using namespace lsq;
+
+typedef double Scalar;
+typedef Eigen::Matrix<Scalar, Eigen::Dynamic, 1> Vector;
+typedef Eigen::Matrix<Scalar, Eigen::Dynamic, Eigen::Dynamic> Matrix;
+
+TEST_CASE("dense SVD solver")
+{
+    const Scalar eps = 1e-6;
+
+    Matrix A(4, 4);
+    A << 2, 3, 11, 5,
+        1, 1, 5, 2,
+        2, 1, -3, 2,
+        1, 1, -3, 4;
+    Vector b(4);
+    b << 2, 1, -3, -3;
+
+    Vector resultAct;
+    Vector resultExp(4);
+    resultExp << -0.5, -0.1875, 0.4375, -0.25;
+
+    DenseSVDSolver<Scalar> solver;
+    solver(A, b, resultAct);
+
+    REQUIRE_MATRIX_APPROX(resultExp, resultAct, eps);
+}
