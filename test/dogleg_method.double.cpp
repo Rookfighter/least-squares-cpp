c34b3597d2a6c3cf70ac28bb1ff1a7a0f35d5441
Fabian Meyer <user8324@posteo.net>
2020-11-21 09:58:17 +0100

    Implement dogleg method tests for different types

---
+++ test/dogleg_method.double.cpp
@@ -0,0 +1,10 @@
+/* dogleg_method.cpp
+ *
+ * Author: Fabian Meyer
+ * Created On: 20 Nov 2020
+ */
+
+#define DATATYPE double
+#define DATATYPE_STR "double"
+
+#include "dogleg_method.in.cpp"
---
+++ test/dogleg_method.float.cpp
@@ -0,0 +1,10 @@
+/* dogleg_method.cpp
+ *
+ * Author: Fabian Meyer
+ * Created On: 20 Nov 2020
+ */
+
+#define DATATYPE float
+#define DATATYPE_STR "float"
+
+#include "dogleg_method.in.cpp"
