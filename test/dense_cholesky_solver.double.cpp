8b087762a40ee0ed1bbbae2dd03602f877d88740
Fabian Meyer <user8324@posteo.net>
2020-11-21 15:54:26 +0100

    Split solvers into separate test files

---
+++ test/dense_cholesky_solver.double.cpp
@@ -0,0 +1,11 @@
+/* dense_svd_solver.cpp
+ *
+ * Author: Fabian Meyer
+ * Created On: 05 Aug 2019
+ */
+
+
+#define DATATYPE double
+#define DATATYPE_STR "double"
+
+#include "dense_cholesky_solver.in.cpp"
---
+++ test/dense_cholesky_solver.float.cpp
@@ -0,0 +1,11 @@
+/* dense_svd_solver.cpp
+ *
+ * Author: Fabian Meyer
+ * Created On: 05 Aug 2019
+ */
+
+
+#define DATATYPE float
+#define DATATYPE_STR "float"
+
+#include "dense_cholesky_solver.in.cpp"
